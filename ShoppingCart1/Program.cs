35925f679b5c8c47a36ac07c90e49206e41184ee
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-02-25 00:10:10 +0900

    finish question 2,3

--- ShoppingCart1/Program.cs
+++ ShoppingCart1/Program.cs
@@ -9,12 +9,13 @@ namespace ShoppingCart1
         {
             // dotnet run 1,2 3,4 5,6
             var list = new List<int>{100, 40, 150, 350, 400, 420, 440, 100, 80, 100};
-            var totalAmount = 0;
+            var tax = new List<double>{1.1, 1.1, 1.1, 1.1, 1.1, 1.0, 1.0, 1.1, 1.1, 1.1};
+            var totalAmount = 0.0;
             foreach(string arg in args){
                 string[] idAndNum = arg.Split(",");
                 if( !int.TryParse(idAndNum[0],out int id) ) continue;
                 if( !int.TryParse(idAndNum[1],out int num) ) continue;
-                totalAmount += list[id - 1] * num;
+                totalAmount += list[id - 1] * num * tax[id - 1];
             }
             Console.WriteLine(totalAmount);
         }
